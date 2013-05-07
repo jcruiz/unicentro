8a816948a71f41835ca5bf51ad750b2dec9c99cb
Juan Camilo Ruiz <jcruiz@gmail.com>
2013-05-06 17:16:19 -0700

    default project

--- scripts/html.ts
+++ scripts/html.ts
@@ -1,31 +1,34 @@
 # HTML Transformations go here
 
 $("/html") {
-  # rewrite_links()
-  # absolutize_srcs()
+  rewrite_links()
+  absolutize_srcs()
 
-  # # Add the mobile meta tags
-  # clean_mobile_meta_tags()
+  # Add the mobile meta tags
+  clean_mobile_meta_tags()
 
-  # # Needed to begin mobilizing
-  # remove_all_styles()
-  # remove_html_comments()
+  # Needed to begin mobilizing
+  remove_all_styles()
+  remove_html_comments()
 
-  # # Late load all the images on the site
-  # # lateload()
+  # Late load all the images on the site
+  # lateload()
 
-  # # Remove all script tags not marked with "data-keep" attribute
-  # # remove_desktop_js();
+  # Remove all script tags not marked with "data-keep" attribute
+  # remove_desktop_js();
 
   
 
-  # add_assets()
+  add_assets()
 
-  # @import "sections/header.ts"
-  # @import "sections/footer.ts"
+  @import "sections/header.ts"
+  @import "sections/footer.ts"
 
-  # @import "mappings.ts"
+  @import "mappings.ts"
 
-  # @import "optimize.ts"
+  @import "optimize.ts"
+  # $$("img") {
+  # attribute("src", "http://www.montessoricats.com/buddha-scottish-fold-its-magic.jpg")
+  # }
 }
 
